c9166990c36e0acce2953aaa45a3eb904a1a6660
Абылай <abylay@MacBook-Air-Abylaj.local>
2023-08-16 20:17:55 +0600

    no cluster

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,6 @@ module.exports = {
     apps: [
         {
             name: "backend-kidup",
-            exec_mode: "cluster",
-            instances: 2,
             script: "./index.js",
         }
     ]
