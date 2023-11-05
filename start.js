2b3c6a34436c8c757fef1cafbceb974ac97047c1
Абылай <abylay@MacBook-Air-Abylaj.local>
2023-11-05 18:33:49 +0600

    add domain cors

--- start.js
+++ start.js
@@ -18,7 +18,7 @@ export default () => {
         limit: '500mb'
     }));
     app.use(cors({
-        origin: ['http://localhost:3000'],
+        origin: ['http://localhost:3000', 'https://kidup.kz'],
         credentials: true,
         methods: "GET, POST, PUT, DELETE",
     }))
