f3a122702313729cf75aaeea430a5f4efb518587
Абылай <abylay@MacBook-Air-Abylaj.local>
2023-11-05 18:55:13 +0600

    cors fix

--- start.js
+++ start.js
@@ -20,7 +20,8 @@ export default () => {
     app.use(cors({
         origin: ['http://localhost:3000', 'http://localhost:3001', 'https://kidup.kz'],
         credentials: true,
-        methods: "GET, POST, PUT, DELETE",
+        preflightContinue: true,
+        methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS'],
     }))
     app.use(cookieParser())
 
