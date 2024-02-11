adbaa01aa2caa6d1b971cc2e1d3e12bf2379b1ad
Абылай <bazarbayev.abylay@gmail.com>
2024-02-11 20:34:13 +0600

    photo toy

--- adminApp/controllers/toys/toy.js
+++ adminApp/controllers/toys/toy.js
@@ -1,5 +1,6 @@
 import models from "../../../models/index.js";
 import {createError, createResponse} from "../../../helpers/responser.js";
+import {removeFile, uploadFile} from "../../../services/image.js";
 
 export const getList = async (req, res) => {
     const roles = await models.Toy.findAll();
@@ -41,4 +42,60 @@ export const deleteToy = async (req, res) => {
     }
 
     res.status(200).json({status: "OK"})
+}
+
+// Добавть картинку
+export const addPhoto = async (req, res) => {
+    const {id} = req.params;
+    const toy = await models.Toy.findOne({where: {id}});
+    if (!toy) return res.status(500).json(createError("Игрушка не найдена"));
+
+    // Массив фоток
+    let photos = toy.dataValues.photos || [];
+
+    const {buffer} = req.body;
+    const filePath = await uploadFile(buffer, "toy");
+    if (!filePath) return res.status(500).json(createError("Не удалось загрузить файл"));
+    photos.push(filePath);
+
+    try {
+        // Сохранение в базе новой картинки
+        await models.Toy.update(
+            {photos},
+            {where: {id}}
+        );
+    } catch (e) {
+        return res.status(500).json(createError("Не удалось сохранить файл"))
+    }
+
+    res.status(200).json({status: "OK"})
+}
+
+// Удалить картинку
+export const removePhoto = async (req, res) => {
+    const {id} = req.params;
+    const toy = await models.Toy.findOne({where: {id}});
+    if (!toy) return res.status(500).json(createError("Игрушка не найдена"));
+
+    const {imagePath} = req.body;
+    if (!imagePath) return res.status(500).json(createError("Нет ссылки на картинку"));
+
+    let photos = toy.dataValues.photos || [];
+    const photoIndex = photos.indexOf(imagePath);
+    if (photoIndex > -1) {
+        await removeFile(imagePath);
+        photos.splice(photoIndex, 1);
+    }
+
+    try {
+        // Сохранение в базе новой картинки
+        await models.Toy.update(
+            {photos},
+            {where: {id}}
+        );
+    } catch (e) {
+        return res.status(500).json(createError("Не удалось сохранить"))
+    }
+
+    res.status(200).json({status: "OK", photos})
 }
--- adminApp/routes/toys/toy.js
+++ adminApp/routes/toys/toy.js
@@ -1,6 +1,6 @@
 import express from "express";
 import onlyAdmin from "../../middlewares/onlyAdmin.js";
-import {getList, createToy, updateToy, deleteToy} from "../../controllers/toys/toy.js";
+import {getList, createToy, updateToy, deleteToy, addPhoto, removePhoto} from "../../controllers/toys/toy.js";
 
 export default () => {
     const router = express.Router();
@@ -9,6 +9,9 @@ export default () => {
     router.post("/create", onlyAdmin, createToy);
     router.put("/update/:id", onlyAdmin, updateToy);
     router.delete("/delete/:id", onlyAdmin, deleteToy);
+
+    router.put("/uploadPhoto/:id", onlyAdmin, addPhoto);
+    router.put("/deletePhoto/:id", onlyAdmin, removePhoto);
 
     return router;
 }
